722a33b8dc62b6b5d19dcd6be4f9bf63e8047e91
kevin-l-tran <kltran@csu.fullerton.edu>
2025-06-09 10:32:30 -0700

    Fixed initializer for SafeWalkVolunteer

--- SafeWalkVolunteer/Volunteer.swift
+++ SafeWalkVolunteer/Volunteer.swift
@@ -33,7 +33,7 @@ class SafeWalkVolunteer: Volunteer {
         self.age = 0
     }
 
-    init(_ name: String, age: Int) {
+    init(name: String, age: Int) {
         self.name = name
         self.age = age
     }
